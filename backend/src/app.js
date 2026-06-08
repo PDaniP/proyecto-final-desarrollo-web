e7fccbc8b38c00bed276c027453b018c4fdc2669
Picasso017 <matiaspicasso017@gmail.com>
2026-06-08 15:32:36 -0300

    creacion provisoria de mangas

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mangasRoutes from './routes/mangas.routes.js';
 import usersRoutes from './routes/users.routes.js';
+import mangaListRoutes from './routes/mangaList.routes.js';
 import cors from 'cors';
 
 const app = express();
@@ -8,6 +9,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/users", usersRoutes);
+app.use("/manga-list", mangaListRoutes);
+app.use("/mangas", mangasRoutes);
 
 
 
@@ -15,7 +18,7 @@ app.get("/", (req, res) => {
     res.send("API funcionando");
 });
 
-app.use("/mangas", mangasRoutes);
+
 
 
 module.exports = app;
--- backend/src/routes/manga.routes.js
+++ backend/src/routes/manga.routes.js
@@ -1,28 +1,13 @@
-const express = require("express")
-const Manga = require('../models/Manga')
+import express from 'express';
+import { addManga,getMangas, getMangaByID} from '../controllers/mangaController.js';
+
 
 const router = express.Router()
 
-router.get("/", async (req, res) => {
-    try {
-        const mangas = await Manga.find()
-        res.json(mangas)
-    } catch (error) {
-        console.error("Error en GET /mangas", error)
-        res.status(500).json({ mensaje: "Error al obtener mangas" })
-    }
-})
+router.post("/", addManga);
+router.get("/", getMangas);
+router.get("/:mangaId", getMangaById);
 
-router.post("/", async (req, res) => {
-    try {
-        const manga = await Manga.create(req.body);
-        res.status(201).json(manga);
-    } catch (error) {
-        res.status(500).json({
-            mensaje: "Error al crear manga",
-            error: error.message
-        });
-    }
-});
+
 
 module.exports = router
